3363b578db40151aaa402c7578805fffd2c93131
kumenger <44558777+kumenger@users.noreply.github.com>
2023-04-02 18:13:19 -0600

    starter code

---
+++ model/User.js
@@ -0,0 +1,41 @@
+
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection.js");
+
+class User extends Model{}
+User.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        first_name:{
+            type: DataTypes.STRING,
+            allowNull:false
+        },
+        last_name:{
+            type: DataTypes.STRING,
+            allowNull:false
+        },
+        email:{
+            type: DataTypes.STRING,
+            allowNull:false
+        },
+        password:{
+            type: DataTypes.STRING,
+            allowNull:false
+        }
+    }
+    ,
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "user",
+      }
+)
+module.exports=User
