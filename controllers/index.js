ed3ba652ab30c4082ec9152ab67ed0cb029662be
kumenger <44558777+kumenger@users.noreply.github.com>
2023-04-04 22:08:08 -0600

    login/logout fixed/

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,11 @@ const router = require('express').Router();
 const userRoutes=require('./api/userRoutes')
 const postRoutes=require('./api/postRoutes')
 const commentRoute=require('./api/comment')
+const homepageRoutes=require('./homeroutes')
+
 router.use('/user',userRoutes)
 router.use('/post',postRoutes)
 router.use('/comment',commentRoute)
+router.use('/',homepageRoutes)
+
 module.exports=router
